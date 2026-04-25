a27898b339ab541761e9a6f6edca51c4e17618f3
German Vel <germanv.dev@gmail.com>
2026-04-25 19:51:31 +0200

    add custom domain

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -58,6 +58,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
+    useHash: true,
     scrollPositionRestoration: 'enabled',
     anchorScrolling: 'enabled'
   })],
